0973ed9aa38afb1e87c90478dc037e6d4d22b52f
iamMarybrightEE <marybrightetim@gmail.com>
2023-08-18 14:13:08 +0100

    initial

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import { useRouteError } from "react-router-dom"
+
+const NotFound = () => {
+  const error = useRouteError()
+  return (
+    <div>{error.message}</div>
+  )
+}
+
+export default NotFound
